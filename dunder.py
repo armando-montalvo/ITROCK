47e9b0c6dc3d5ad6bfb7b489cb19cc531a9018a4
marioBZ7 <mario.briseno7@gmail.com>
2017-06-29 11:17:22 -0500

    Hola

---
+++ dunder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 14:41:30 2017
+
+@author: Mario
+"""
+
+def pr():
+    
+    print('We start off in:',__name__)
+    if __name__ == '__main__':
+        print('And end up in:',__name__)
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 09:46:45 2017
+
+@author: Mario
+"""
+
+from setuptools import setup
+
+setup(
+        name='vsearch',
+        version='1.0',
+        description='just something nice',
+        author='AMG',
+        author_email='a@mg.com',
+        url='artlabs.xyz',
+        py_modules=['vsearch']
+        )
+
+
+]
